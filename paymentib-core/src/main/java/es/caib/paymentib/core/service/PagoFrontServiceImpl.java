ece8e55037e47c31baa4c007984e25a036234f85
jmico-indra <jmico@indra.es>
2019-06-03 12:06:07 +0200

    #4 Cambios realizados:
    - Subida de versión de la 0.0.1-SNAPSHOT a la 1.0.0

--- paymentib-core/src/main/java/es/caib/paymentib/core/service/PagoFrontServiceImpl.java
+++ paymentib-core/src/main/java/es/caib/paymentib/core/service/PagoFrontServiceImpl.java
@@ -40,263 +40,248 @@ import es.caib.paymentib.plugins.api.UrlRedireccionPasarelaPago;
 @Transactional
 public final class PagoFrontServiceImpl implements PagoFrontService {
 
-    // TODO Meter en las excepciones el identificador del pago
+	// TODO Meter en las excepciones el identificador del pago
 
-    /** Log. */
-    private final org.slf4j.Logger log = LoggerFactory
-            .getLogger(PagoFrontServiceImpl.class);
+	/** Log. */
+	private final org.slf4j.Logger log = LoggerFactory.getLogger(PagoFrontServiceImpl.class);
 
-    /** Configuracion. */
-    @Autowired
-    private ConfiguracionComponent config;
+	/** Configuracion. */
+	@Autowired
+	private ConfiguracionComponent config;
 
-    /** Dao. */
-    @Autowired
-    private PagoDao dao;
+	/** Dao. */
+	@Autowired
+	private PagoDao dao;
 
-    @Override
-    public DatosSesionPago crearPagoElectronico(String pasarelaId,
-            DatosPago datosPago, String urlCallbackAppOrigen) {
+	@Override
+	public DatosSesionPago crearPagoElectronico(final String pasarelaId, final DatosPago datosPago,
+			final String urlCallbackAppOrigen) {
 
-        // Almacenar en persistencia los datos del pago y retorna token acceso
-        final String tokenAcceso = GeneradorId.generarId();
-        final String identificador = dao.create(pasarelaId, datosPago,
-                urlCallbackAppOrigen, tokenAcceso);
-        log.debug("Creado pago electronico con identificador: " + identificador
-                + " y token acceso: " + tokenAcceso);
+		// Almacenar en persistencia los datos del pago y retorna token acceso
+		final String tokenAcceso = GeneradorId.generarId();
+		final String identificador = dao.create(pasarelaId, datosPago, urlCallbackAppOrigen, tokenAcceso);
+		log.debug("Creado pago electronico con identificador: " + identificador + " y token acceso: " + tokenAcceso);
 
-        return this.recuperarSesionPagoByIdentificador(identificador);
-    }
+		return this.recuperarSesionPagoByIdentificador(identificador);
+	}
 
-    @Override
-    public DatosSesionPago recuperarPagoElectronico(String tokenSesion) {
-        log.debug("Recuperando pago con token: " + tokenSesion);
-        // Recuperamos sesion pago
-        final DatosSesionPago dp = recuperarSesionPagoByToken(tokenSesion);
-        log.debug("Recuperado pago  " + dp.getDatosPago().getIdentificador());
-        // Retornamos datos sesión pago
-        return dp;
-    }
+	@Override
+	public DatosSesionPago recuperarPagoElectronico(final String tokenSesion) {
+		log.debug("Recuperando pago con token: " + tokenSesion);
+		// Recuperamos sesion pago
+		final DatosSesionPago dp = recuperarSesionPagoByToken(tokenSesion);
+		log.debug("Recuperado pago  " + dp.getDatosPago().getIdentificador());
+		// Retornamos datos sesión pago
+		return dp;
+	}
 
-    // TODO Renombrar a RedirigirPasarelaPago
-    @Override
-    public UrlRedireccionPasarelaPago iniciarPagoElectronico(
-            String identificador, String entidadPagoId,
-            String urlCallbackCompPagos) {
+	// TODO Renombrar a RedirigirPasarelaPago
+	@Override
+	public UrlRedireccionPasarelaPago iniciarPagoElectronico(final String identificador, final String entidadPagoId,
+			final String urlCallbackCompPagos) {
 
-        // Recuperamos sesion pago
-        final DatosSesionPago dp = recuperarSesionPagoByIdentificador(
-                identificador);
-        if (dp.getEstado() != TypeEstadoPago.NO_INICIADO) {
-            throw new EstadoSesionPagoException(
-                    "El estado debe ser no iniciado");
-        }
+		// Recuperamos sesion pago
+		final DatosSesionPago dp = recuperarSesionPagoByIdentificador(identificador);
+		if (dp.getEstado() != TypeEstadoPago.NO_INICIADO) {
+			throw new EstadoSesionPagoException("El estado debe ser no iniciado");
+		}
 
-        // Genera token y anexa a url callback
-        final String token = GeneradorId.generarId();
-        final String urlCallbackToken = urlCallbackCompPagos + "/" + token
-                + ".html";
+		// Genera token y anexa a url callback
+		final String token = GeneradorId.generarId();
+		final String urlCallbackToken = urlCallbackCompPagos + "/" + token + ".html";
 
-        // Crea plugin pago
-        final IPasarelaPagoPlugin plgPago = crearPlugin(dp.getPasarelaId());
+		// Crea plugin pago
+		final IPasarelaPagoPlugin plgPago = crearPlugin(dp.getPasarelaId());
 
-        // Genera pago en pasarela
-        UrlRedireccionPasarelaPago url = null;
-        try {
-            url = plgPago.iniciarPagoElectronico(dp.getDatosPago(),
-                    entidadPagoId, urlCallbackToken);
-        } catch (final PasarelaPagoException e) {
-            throw new InicioPagoException(e);
-        }
+		// Genera pago en pasarela
+		UrlRedireccionPasarelaPago url = null;
+		try {
+			url = plgPago.iniciarPagoElectronico(dp.getDatosPago(), entidadPagoId, urlCallbackToken);
+		} catch (final PasarelaPagoException e) {
+			throw new InicioPagoException(e);
+		}
 
-        // Almacena pago en persistencia cambiando estado
-        // TODO Almacenar entidadPagoId??
-        dao.iniciar(dp.getDatosPago().getIdentificador(), url.getLocalizador(),
-                token);
+		// Almacena pago en persistencia cambiando estado
+		// TODO Almacenar entidadPagoId??
+		dao.iniciar(dp.getDatosPago().getIdentificador(), url.getLocalizador(), token);
 
-        return url;
+		return url;
 
-    }
+	}
 
-    @Override
-    public List<EntidadPago> obtenerEntidadesPagoElectronico(
-            String identificador) {
+	@Override
+	public List<EntidadPago> obtenerEntidadesPagoElectronico(final String identificador) {
 
-        // Recuperamos sesion pago
-        final DatosSesionPago dp = recuperarSesionPagoByIdentificador(
-                identificador);
-        if (dp == null) {
-            throw new NoExisteSesionPagoException(identificador);
-        }
+		// Recuperamos sesion pago
+		final DatosSesionPago dp = recuperarSesionPagoByIdentificador(identificador);
+		if (dp == null) {
+			throw new NoExisteSesionPagoException(identificador);
+		}
 
-        // Crea plugin pago
-        final IPasarelaPagoPlugin plgPago = crearPlugin(dp.getPasarelaId());
+		// Crea plugin pago
+		final IPasarelaPagoPlugin plgPago = crearPlugin(dp.getPasarelaId());
 
-        // Obtiene entidades de pago
-        try {
-            return plgPago.obtenerEntidadesPagoElectronico(
-                    dp.getDatosPago().getIdioma());
-        } catch (final PasarelaPagoException e) {
-            throw new ObtenerEntidadesException(e);
-        }
-    }
+		// Obtiene entidades de pago
+		try {
+			return plgPago.obtenerEntidadesPagoElectronico(dp.getDatosPago().getIdioma());
+		} catch (final PasarelaPagoException e) {
+			throw new ObtenerEntidadesException(e);
+		}
+	}
 
-    @Override
-    public EstadoPago verificarRetornoPagoElectronico(String identificador,
-            Map<String, String[]> parametrosRetorno) {
-        final EstadoPago res = verificarPagoImpl(identificador, true,
-                parametrosRetorno);
-        return res;
-    }
+	@Override
+	public EstadoPago verificarRetornoPagoElectronico(final String identificador,
+			final Map<String, String[]> parametrosRetorno) {
+		final EstadoPago res = verificarPagoImpl(identificador, true, parametrosRetorno);
+		return res;
+	}
 
-    @Override
-    public EstadoPago verificarPagoElectronico(String identificador) {
-        final EstadoPago res = verificarPagoImpl(identificador, false, null);
-        return res;
-    }
+	@Override
+	public EstadoPago verificarPagoElectronico(final String identificador) {
+		final EstadoPago res = verificarPagoImpl(identificador, false, null);
+		return res;
+	}
 
-    @Override
-    public byte[] obtenerJustificantePagoElectronico(String identificador) {
-        // Recuperamos sesion pago
-        final DatosSesionPago dp = recuperarSesionPagoByIdentificador(
-                identificador);
-        if (dp == null) {
-            throw new NoExisteSesionPagoException(identificador);
-        }
+	@Override
+	public byte[] obtenerJustificantePagoElectronico(final String identificador) {
+		// Recuperamos sesion pago
+		final DatosSesionPago dp = recuperarSesionPagoByIdentificador(identificador);
+		if (dp == null) {
+			throw new NoExisteSesionPagoException(identificador);
+		}
 
-        // Verificamos estado pagado
-        if (dp.getEstado() != TypeEstadoPago.PAGADO) {
-            throw new EstadoSesionPagoException("El pago no está completado");
-        }
+		// Verificamos estado pagado
+		if (dp.getEstado() != TypeEstadoPago.PAGADO) {
+			throw new EstadoSesionPagoException("El pago no está completado");
+		}
 
-        // Crea plugin pago
-        final IPasarelaPagoPlugin plgPago = crearPlugin(dp.getPasarelaId());
+		// Crea plugin pago
+		final IPasarelaPagoPlugin plgPago = crearPlugin(dp.getPasarelaId());
 
-        // Obtiene justificante
-        byte[] justif = null;
-        try {
-            justif = plgPago.obtenerJustificantePagoElectronico(
-                    dp.getDatosPago(), dp.getLocalizador());
+		// Obtiene justificante
+		byte[] justif = null;
+		try {
+			justif = plgPago.obtenerJustificantePagoElectronico(dp.getDatosPago(), dp.getLocalizador());
 
-            // Si la pasarela no provee justificante, proveemos justificante
-            // genérico
-            if (justif == null) {
-                justif = GeneradorJustificantePago.generarJustificantePago(
-                        config.obtenerDirectorioConfiguracion(), dp);
-            }
+			// Si la pasarela no provee justificante, proveemos justificante
+			// genérico
+			if (justif == null) {
+				justif = GeneradorJustificantePago.generarJustificantePago(config.obtenerDirectorioConfiguracion(), dp);
+			}
 
-        } catch (final PasarelaPagoException e) {
-            throw new JustificantePagoException(e);
-        }
+		} catch (final PasarelaPagoException e) {
+			throw new JustificantePagoException(e);
+		}
 
-        return justif;
+		return justif;
 
-    }
+	}
 
-    @Override
-    public int consultaTasa(String pasarelaId, String idTasa) {
-        // Crea plugin pago
-        final IPasarelaPagoPlugin plgPago = crearPlugin(pasarelaId);
-        // Consulta tasa
-        try {
-            return plgPago.consultaTasa(idTasa);
-        } catch (final PasarelaPagoException e) {
-            throw new ConsultarTasaException(idTasa, e);
-        }
-    }
+	@Override
+	public int consultaTasa(final String pasarelaId, final String idTasa) {
+		// Crea plugin pago
+		final IPasarelaPagoPlugin plgPago = crearPlugin(pasarelaId);
+		// Consulta tasa
+		try {
+			return plgPago.consultaTasa(idTasa);
+		} catch (final PasarelaPagoException e) {
+			throw new ConsultarTasaException(idTasa, e);
+		}
+	}
 
-    @Override
-    public byte[] obtenerCartaPagoPresencial(String pasarelaId,
-            DatosPago datosPago) {
-        // Crea plugin pago
-        final IPasarelaPagoPlugin plgPago = crearPlugin(pasarelaId);
-        // Consulta tasa
-        try {
-            return plgPago.obtenerCartaPagoPresencial(datosPago);
-        } catch (final PasarelaPagoException e) {
-            throw new CartaPagoException(e);
-        }
-    }
+	@Override
+	public boolean permitePagoPresencial(final String pasarelaId) {
+		// Crea plugin pago
+		final IPasarelaPagoPlugin plgPago = crearPlugin(pasarelaId);
+		// Consulta si permite paso presencial
+		return plgPago.permitePagoPresencial();
+	}
 
-    @Override
-    public String obtenerUrlFrontal() {
-        return config.obtenerPropiedadConfiguracion("frontal.url");
-    }
+	@Override
+	public byte[] obtenerCartaPagoPresencial(final String pasarelaId, final DatosPago datosPago) {
+		// Crea plugin pago
+		final IPasarelaPagoPlugin plgPago = crearPlugin(pasarelaId);
+		// Consulta tasa
+		try {
+			return plgPago.obtenerCartaPagoPresencial(datosPago);
+		} catch (final PasarelaPagoException e) {
+			throw new CartaPagoException(e);
+		}
+	}
 
-    // --------------------------------------------------------
-    // Funciones privadas
-    // --------------------------------------------------------
+	@Override
+	public String obtenerUrlFrontal() {
+		return config.obtenerPropiedadConfiguracion("frontal.url");
+	}
 
-    /**
-     * Crea plugin pasarela pago.
-     *
-     * @param pasarelaId
-     *            id pasarela
-     * @return plugin pago
-     */
-    private IPasarelaPagoPlugin crearPlugin(String pasarelaId) {
-        final IPasarelaPagoPlugin plgPago = config
-                .obtenerPluginPasarelaPago(pasarelaId);
-        return plgPago;
-    }
+	// --------------------------------------------------------
+	// Funciones privadas
+	// --------------------------------------------------------
 
-    private DatosSesionPago recuperarSesionPagoByToken(String tokenSesion) {
-        final DatosSesionPago dp = dao.getByToken(tokenSesion);
-        if (dp == null) {
-            throw new TokenSesionPagoException(tokenSesion);
-        }
-        return dp;
-    }
+	/**
+	 * Crea plugin pasarela pago.
+	 *
+	 * @param pasarelaId
+	 *            id pasarela
+	 * @return plugin pago
+	 */
+	private IPasarelaPagoPlugin crearPlugin(final String pasarelaId) {
+		final IPasarelaPagoPlugin plgPago = config.obtenerPluginPasarelaPago(pasarelaId);
+		return plgPago;
+	}
 
-    private DatosSesionPago recuperarSesionPagoByIdentificador(
-            String identificador) {
-        final DatosSesionPago dp = dao.getByIdentificador(identificador);
-        if (dp == null) {
-            throw new NoExisteSesionPagoException(identificador);
-        }
-        return dp;
-    }
+	private DatosSesionPago recuperarSesionPagoByToken(final String tokenSesion) {
+		final DatosSesionPago dp = dao.getByToken(tokenSesion);
+		if (dp == null) {
+			throw new TokenSesionPagoException(tokenSesion);
+		}
+		return dp;
+	}
 
-    private EstadoPago verificarPagoImpl(String identificador,
-            boolean retornoPago, Map<String, String[]> parametrosRetorno) {
-        EstadoPago res = null;
-        // Recuperamos sesion pago
-        final DatosSesionPago dp = recuperarSesionPagoByIdentificador(
-                identificador);
-        // Si no esta iniciado o si ya esta pagado, establecemos estado de
-        // persistencia
-        if (dp.getEstado() == TypeEstadoPago.NO_INICIADO
-                || dp.getEstado() == TypeEstadoPago.PAGADO) {
-            res = new EstadoPago();
-            res.setEstado(dp.getEstado());
-            res.setLocalizador(dp.getLocalizador());
-            res.setFechaPago(dp.getFechaPago());
-            res.setCodigoErrorPasarela(dp.getCodigoErrorPasarela());
-            res.setMensajeErrorPasarela(dp.getMensajeErrorPasarela());
-        } else {
-            // Verificamos estado contra la pasarela
-            // Crea plugin pago
-            final IPasarelaPagoPlugin plgPago = crearPlugin(dp.getPasarelaId());
-            try {
-                // Verifica estado pago según si es retorno o una verifación
-                // normal
-                EstadoPago ep = null;
-                if (retornoPago) {
-                    ep = plgPago.verificarRetornoPagoElectronico(
-                            dp.getDatosPago(), dp.getLocalizador(),
-                            parametrosRetorno);
-                } else {
-                    ep = plgPago.verificarPagoElectronico(dp.getDatosPago(),
-                            dp.getLocalizador());
-                }
-                // Actualizamos estado
-                dao.actualizarEstado(identificador, ep);
-                res = ep;
-            } catch (final PasarelaPagoException e) {
-                throw new VerificacionPagoException(identificador, e);
-            }
-        }
-        return res;
-    }
+	private DatosSesionPago recuperarSesionPagoByIdentificador(final String identificador) {
+		final DatosSesionPago dp = dao.getByIdentificador(identificador);
+		if (dp == null) {
+			throw new NoExisteSesionPagoException(identificador);
+		}
+		return dp;
+	}
+
+	private EstadoPago verificarPagoImpl(final String identificador, final boolean retornoPago,
+			final Map<String, String[]> parametrosRetorno) {
+		EstadoPago res = null;
+		// Recuperamos sesion pago
+		final DatosSesionPago dp = recuperarSesionPagoByIdentificador(identificador);
+		// Si no esta iniciado o si ya esta pagado, establecemos estado de
+		// persistencia
+		if (dp.getEstado() == TypeEstadoPago.NO_INICIADO || dp.getEstado() == TypeEstadoPago.PAGADO) {
+			res = new EstadoPago();
+			res.setEstado(dp.getEstado());
+			res.setLocalizador(dp.getLocalizador());
+			res.setFechaPago(dp.getFechaPago());
+			res.setCodigoErrorPasarela(dp.getCodigoErrorPasarela());
+			res.setMensajeErrorPasarela(dp.getMensajeErrorPasarela());
+		} else {
+			// Verificamos estado contra la pasarela
+			// Crea plugin pago
+			final IPasarelaPagoPlugin plgPago = crearPlugin(dp.getPasarelaId());
+			try {
+				// Verifica estado pago según si es retorno o una verifación
+				// normal
+				EstadoPago ep = null;
+				if (retornoPago) {
+					ep = plgPago.verificarRetornoPagoElectronico(dp.getDatosPago(), dp.getLocalizador(),
+							parametrosRetorno);
+				} else {
+					ep = plgPago.verificarPagoElectronico(dp.getDatosPago(), dp.getLocalizador());
+				}
+				// Actualizamos estado
+				dao.actualizarEstado(identificador, ep);
+				res = ep;
+			} catch (final PasarelaPagoException e) {
+				throw new VerificacionPagoException(identificador, e);
+			}
+		}
+		return res;
+	}
 
 }

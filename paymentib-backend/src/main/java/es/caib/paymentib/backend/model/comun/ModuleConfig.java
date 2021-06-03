aec23a3f7957a271a06bb89e521b6db31f765f8f
slromero-indra <slromero@indra.es>
2021-06-03 16:17:33 +0200

    Faltaba actualizar el fichero

--- paymentib-backend/src/main/java/es/caib/paymentib/backend/model/comun/ModuleConfig.java
+++ paymentib-backend/src/main/java/es/caib/paymentib/backend/model/comun/ModuleConfig.java
@@ -12,6 +12,15 @@ public final class ModuleConfig {
 	/** Entorno. **/
 	private String entorno;
 
+	/** Version. **/
+	private String version;
+
+	/** Commit svn. **/
+	private String commitSvn;
+
+	/** Commit git. **/
+	private String commitGit;
+
 	/**
 	 * @return the entorno
 	 */
@@ -20,11 +29,34 @@ public final class ModuleConfig {
 	}
 
 	/**
-	 * @param entorno
-	 *            the entorno to set
+	 * @param entorno the entorno to set
 	 */
 	public void setEntorno(final String entorno) {
 		this.entorno = entorno;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(final String version) {
+		this.version = version;
+	}
+
+	public String getCommitSvn() {
+		return commitSvn;
+	}
+
+	public void setCommitSvn(final String commitSvn) {
+		this.commitSvn = commitSvn;
+	}
+
+	public String getCommitGit() {
+		return commitGit;
+	}
+
+	public void setCommitGit(final String commitGit) {
+		this.commitGit = commitGit;
 	}
 
 }
